4ba62d5a5f8a49a0dd956c7853b36ae636ff1c6d
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-14 20:34:10 +0600

    c1-camp4

---
+++ myfamily.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq ' .[] | select( .id == '$HERO_ID') | .connections .relatives' | cut -d '"' -f2
+
