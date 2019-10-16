33fed65e768d684a0fcca8f0a39c0884536655c6
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-16 16:21:58 +0600

    printNbr

---
+++ printnbr.go
@@ -0,0 +1,22 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int) {
+	var q int = 1
+	if n < 0 {
+		z01.PrintRune('-')
+		q = -1
+		n = n * q
+	}
+	if n < 10 {
+		z01.PrintRune(rune(n+48))
+	}
+	var a int = 0
+	if n > 10 {
+		a = n % 10
+		PrintNbr(n/10)
+		z01.PrintRune(rune(a+48))
+	}
+	// z01.PrintRune(10)
+}
