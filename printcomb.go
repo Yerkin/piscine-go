92a86f45f97eefd681e290f7dd467dd2b222a350
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-16 13:31:55 +0600

    printComb

--- printcomb.go
+++ printcomb.go
@@ -3,33 +3,22 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	var a rune = 48
-	var b rune = 49
-	var c rune = 50
-	var i rune = 1
-	for a <= 55 {
-		for b <= 56 {
-			for c <= 57 {
-				if a < b && b < c {
-					z01.PrintRune(a)
-					z01.PrintRune(b)
-					z01.PrintRune(c)
-					if a < 55 {
+	for x := '0'; x <= '7'; x++ {
+		for y := '1'; y <= '8'; y++ {
+			for z := '2'; z <= '9'; z++ {
+				if x < y && y < z {
+					z01.PrintRune(x)
+					z01.PrintRune(y)
+					z01.PrintRune(z)
+					if x < '7' {
 						z01.PrintRune(44)
 						z01.PrintRune(32)
-						}
-				}
 
-				c = c + i
+					}
+
 				}
-			c = 48
-			b = b + i
 			}
-			b = 48
-			a = a + i
-			b = a + i
-			c = c + i
 		}
-	z01.PrintRune(10)		
 	}
-
+	z01.PrintRune(10)
+}
