97c0a6c47e2f0de237394879f78fa1d3a2b0c02b
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-16 11:49:45 +0600

    printAlphabet

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-    "github.com/01-edu/z01"
+    	"github.com/01-edu/z01"
 )
 
 func main() {
 
-    for i := 'a'; i <= 'z'; i++{
-        z01.PrintRune(i)
-    }
-    z01.PrintRune('\n')
+    	for i := 'a'; i <= 'z'; i++{
+        	z01.PrintRune(i)
+    	}
+    	z01.PrintRune('\n')
 }
