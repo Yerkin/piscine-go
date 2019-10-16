794c02080ac44e525904b16108d4b503315c2953
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-16 17:00:19 +0600

    printNbr

--- printnbr.go
+++ printnbr.go
@@ -5,11 +5,11 @@ import "github.com/01-edu/z01"
 func PrintNbr(n int) {
 	var q int = 1
 	if n < 0 {
-		z01.PrintRune('-')
+		z01.PrintRune(45)
 		q = -1
 		n = n * q
 	}
-	if n < 10 {
+	if n < 10 && n >= 0 {
 		z01.PrintRune(rune(n + 48))
 	}
 	var a int = 0
