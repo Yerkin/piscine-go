226ecb84382365e31410cb91b659daf0e2d8ca84
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-16 16:23:02 +0600

    printNbr

--- printnbr.go
+++ printnbr.go
@@ -10,13 +10,13 @@ func PrintNbr(n int) {
 		n = n * q
 	}
 	if n < 10 {
-		z01.PrintRune(rune(n+48))
+		z01.PrintRune(rune(n + 48))
 	}
 	var a int = 0
 	if n > 10 {
 		a = n % 10
-		PrintNbr(n/10)
-		z01.PrintRune(rune(a+48))
+		PrintNbr(n / 10)
+		z01.PrintRune(rune(a + 48))
 	}
 	// z01.PrintRune(10)
 }
