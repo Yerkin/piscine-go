27ce4753ebd72d61139a40aa7ffeae8c39e82cf1
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-16 14:04:38 +0600

    printComb2

--- printcomb2.go
+++ printcomb2.go
@@ -7,7 +7,7 @@ func PrintComb2() {
 		for b := '0'; b <= '9'; b++ {
 			for c := '0'; c <= '9'; c++ {
 				for d := '0'; d <= '9'; d++ {
-					if a <= c && b < d { 
+					if a <= c && b < d {
 						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(32)
@@ -17,7 +17,7 @@ func PrintComb2() {
 							z01.PrintRune(44)
 						}
 					}
-				} 
+				}
 			}
 		}
 	}
