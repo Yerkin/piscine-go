300457908b27216250920ea8574051dc193fd379
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-17 13:42:45 +0600

    quest3-9

---
+++ basicatoi.go
@@ -0,0 +1,17 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	o_number := 0
+	c := 0
+	a_s := []rune(s)
+	for _, word := range a_s {
+		for i := '0'; i < word; i++ {
+			c++
+		}
+		o_number = o_number*10 + c
+		c = 0
+	}
+	return o_number
+}
+
+	
