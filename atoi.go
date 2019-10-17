d4239e8eed751b675033daf9d5cce0694da585fd
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-17 14:10:46 +0600

    quest3-10

---
+++ atoi.go
@@ -0,0 +1,31 @@
+package piscine
+
+func Atoi(s string) int {
+	o_number := 0
+	c := 0
+	checker := true
+	a_s := []rune(s)
+	pl := 1
+	if byte(a_s[0]) == 45 {
+		pl = -1
+		a_s[0] = '0'
+	} else if a_s[0] == 43 {
+		a_s[0] = '0'
+	}
+	for _, word := range a_s {
+		if byte(word) >= 48 && byte(word) <= 57 {
+			for i := '0'; i < word; i++ {
+				c++
+			}
+			o_number = o_number*10 + c
+			c = 0
+		} else {
+			checker = false
+		}
+	}
+	if checker {
+		return o_number * pl
+	} else {
+		return 0
+	}
+}
