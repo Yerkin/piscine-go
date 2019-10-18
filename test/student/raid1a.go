43883fd4b9a7c7f686e88b825572ecb5e97db651
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-18 19:32:54 +0600

    raid1-01

--- test/student/raid1a.go
+++ test/student/raid1a.go
@@ -5,28 +5,45 @@ import (
 )
 
 func Raid1a(x, y int) {
-	for i := 0; i < y; i++ {
-		for j := 0; j < x; j++ {
-			if i == 0 && j == 0 {
-				z01.PrintRune('o')
-			} else if i == 0 && j == x-1 {
-				z01.PrintRune('o')
-			} else if i == y-1 && j == x-1 {
-				z01.PrintRune('o')
-			} else if i == y-1 && j == 0 {
-				z01.PrintRune(10)
-				z01.PrintRune('o')
-			} else if (i != 0 && i != y-1) && (j == 0) {
-				z01.PrintRune(10)
-				z01.PrintRune('|')
-			} else if (i != 0 && i != y-1) && (j == x-1) {
-				z01.PrintRune('|')
-			} else if i == 0 || i == y-1 {
-				z01.PrintRune('-')
-			} else {
-				z01.PrintRune(32)
+	if x > 0 && y > 0 {
+		for i := 0; i < y; i++ {
+			for j := 0; j < x; j++ {
+				if i == 0 && j == 0 {
+					z01.PrintRune('o')
+					if j == x-1 {
+						z01.PrintRune(10)
+					}
+
+				} else if i == 0 && j == x-1 {
+					z01.PrintRune('o')
+					if i != y-1 {
+						z01.PrintRune(10)
+					}
+
+				} else if i == y-1 && j == x-1 {
+					z01.PrintRune('o')
+
+				} else if i == y-1 && j == 0 {
+					z01.PrintRune('o')
+					//wall
+				} else if (i != 0 && i != y-1) && (j == 0) {
+					z01.PrintRune('|')
+					if j == x-1 {
+						z01.PrintRune(10)
+					}
+				} else if (i != 0 && i != y-1) && (j == x-1) && (x != 1) {
+					z01.PrintRune('|')
+					z01.PrintRune(10)
+
+				} else if i == 0 || i == y-1 {
+					z01.PrintRune('-')
+				} else {
+					z01.PrintRune(32)
+				}
 			}
 		}
+		z01.PrintRune(10)
+	} else {
+		z01.PrintRune(10)
 	}
-	z01.PrintRune(10)
 }
