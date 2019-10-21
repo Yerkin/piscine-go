f28ed779048c2ebab8eeeb4f7ca3e59cff12af13
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-21 17:26:59 +0600

    quest4-9

---
+++ eightqueens.go
@@ -0,0 +1,57 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+var ans [9]rune
+var ban [9]bool
+var ans2 [9]int
+
+func EightQueens() {
+	ok := true
+	cnt := 0
+	for i := 1; i <= 8; i++ {
+		if ban[i] == false {
+			ok = false
+			//	break
+		} else {
+			cnt++
+		}
+	}
+
+	if ok == true {
+		for _, c := range ans {
+			z01.PrintRune(c)
+		}
+		z01.PrintRune('\n')
+
+		return
+	}
+	for i := '1'; i <= '8'; i++ {
+		cur := 0
+		for j := '1'; j <= i; j++ {
+			cur++
+		}
+		if ban[cur] == false {
+			put := true
+			for j := 1; j <= cnt; j++ {
+				if cur == ans2[j]-(cnt+1-j) || cur == ans2[j]+(cnt+1-j) {
+					put = false
+					break
+				} /*
+					fmt.Print(ans2[j])
+					fmt.Print(" ")*/
+
+			}
+			//	fmt.Println("    <<< THIS SHIT")
+			if put == true {
+				ban[cur] = true
+				ans[cnt+1] = i
+				ans2[cnt+1] = cur
+				EightQueens()
+				ban[cur] = false
+				//ans2[cnt+1] = 0
+
+			}
+		}
+	}
+}
