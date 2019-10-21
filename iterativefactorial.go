a11decfc5bcfae4662c43b98f756b8fe1e69ab7f
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-21 12:09:44 +0600

    quest4-1

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,8 +2,14 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	a := 1
-	for i := 1; i <= nb; i++ {
-		a = a * i
+	if nb == 1 {
+		return 1
+	} else if nb <= 0 {
+		return 0
+	} else if nb > 1 || nb <= 25 {
+		for i := 1; i <= nb; i++ {
+			a = a * i
+		}
 	}
 
 	return a
