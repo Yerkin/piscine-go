e5ff1e94bdab06ae4af685e8fe2a8067aa388824
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-21 13:15:03 +0600

    quest4-8

--- findnextprime.go
+++ findnextprime.go
@@ -1,14 +1,22 @@
 package piscine
 
-func FindNextPrime(nb int) int {
-	if IsPrime(nb) {
-		return nb
-	} else {
-		for {
-			if IsPrime(nb) {
-				return nb
-			}
-			nb = nb + 1
+func check(x int) bool {
+	if x <= 1 {
+		return false
+	}
+	for i := 2; i*i <= x; i++ {
+		if x%i == 0 {
+			return false
 		}
+	}
+	return true
+}
+
+func FindNextPrime(nb int) int {
+	for {
+		if check(nb) {
+			return nb
+		}
+		nb++
 	}
 }
