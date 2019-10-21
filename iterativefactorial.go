1345acadf59d22e0b517313edbe560dc0e1566d4
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-21 12:11:43 +0600

    quest4-1

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,12 +4,12 @@ func IterativeFactorial(nb int) int {
 	a := 1
 	if nb == 1 {
 		return 1
-	} else if nb <= 0 {
-		return 0
-	} else if nb > 1 || nb <= 25 {
+	} else if nb > 1 && nb <= 25 {
 		for i := 1; i <= nb; i++ {
 			a = a * i
 		}
+	} else {
+		return 0
 	}
 
 	return a
