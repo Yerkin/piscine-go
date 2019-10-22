c0ab6862793cdb1f9768e8dcf8dbc1d409ec53b0
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-22 16:55:44 +0600

    quest5-5

--- nrune.go
+++ nrune.go
@@ -1,6 +1,14 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	my_arr := []rune(s)
-	return (my_arr[n-1])
+	my_c := 0
+	for index := range s {
+		my_c = index
+	}
+	if (my_c + 1) >= n {
+		my_arr := []rune(s)
+		return (my_arr[n-1])
+	} else {
+		return 0
+	}
 }
