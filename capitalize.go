3564c09172c8fad84178b81610a59462b884dbde
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-22 18:28:42 +0600

    quest5-11

---
+++ capitalize.go
@@ -0,0 +1,22 @@
+package piscine
+
+func Capitalize(s string) string {
+	my_arr := []rune(s)
+	if my_arr[0] >= 'a' && my_arr[0] <= 'z' {
+		my_arr[0] = rune(my_arr[0] - 32)
+	}
+
+	len_a := 0
+
+	for index := range my_arr {
+		len_a = index
+	}
+
+	for i := 1; i <= len_a; i++ {
+		if (my_arr[i-1] < 'A' || my_arr[i-1] > 'Z') && (my_arr[i] >= 'a' && my_arr[i] <= 'z') && (my_arr[i-1] < 'a' || my_arr[i-1] > 'z') && (my_arr[i-1] < '0' || my_arr[i-1] > '9') {
+			my_arr[i] = rune(my_arr[i] - 32)
+		}
+	}
+
+	return string(my_arr)
+}
