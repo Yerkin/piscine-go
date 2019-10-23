6915d5bc42a0366dbff3ebb22f0606101685ec36
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-23 14:57:45 +0600

    quest5-20

--- printnbrbase.go
+++ printnbrbase.go
@@ -7,30 +7,26 @@ import (
 func PrintNbrBase(s int, t string) {
 	ans := ""
 	ln := 0
-
-	for i := range t {
-		ln = i
+	for _, c := range t {
+		if c == c {
+			ln++
+		}
 	}
-
 	mx_p := ln
-
 	if s < 0 {
 		ans = "-"
 		mx_p *= -1
 	}
-
 	if ln > 1 {
 
 		for s/mx_p >= ln {
 			mx_p *= ln
 		}
-
 		for mx_p != 0 {
 			ans = ans + string(t[s/mx_p])
 			s = s - s/mx_p*mx_p
 			mx_p /= ln
 		}
-
 		x := map[rune]bool{}
 		for _, c := range t {
 			if c == '+' || c == '-' {
