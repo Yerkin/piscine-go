7387f09bc4b6a0982a7041835ff33fd5acd2c001
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-23 15:16:06 +0600

    quest6-1

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	fmt.Println(os.Args[0])
+	my_args := os.Args[0]
+	my_arr := []rune(my_args)
+	ln := 0
+	for i := range my_args {
+		ln = i
+	}
+	for i := 0; i <= ln; i++ {
+		z01.PrintRune(rune(my_arr[i]))
+	}
+	z01.PrintRune('\n')
 }
