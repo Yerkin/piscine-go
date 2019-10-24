18d03abf9be8a50f26395208f60fa38eb561a152
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-24 18:53:25 +0600

    quest7-6

--- convertbase.go
+++ convertbase.go
@@ -1,38 +1,30 @@
 package piscine
 
-func ConvertBase(nbr, baseFrom, baseTo string) string {
-	ln1 := 0
+func ConvertBase(s, t, p string) string {
+	ln := 0
 	ln2 := 0
 	ln3 := 0
 	ans := ""
-
-	for i := range nbr {
-		ln1 = i
+	st_t := map[rune]int{}
+	for c := range s {
+		ln = c + 1
 	}
-
-	arr1 := []rune(nbr)
-
-	for i := range baseFrom {
-		ln2 = i
+	for i, c := range t {
+		st_t[c] = i
+		ln2 = i + 1
 	}
-	ln2++
-
-	for i := range baseTo {
-		ln3 = i + 1
+	for c := range p {
+		ln3 = c + 1
 	}
-
-	myNum := 0
-	pow := 1
-
-	for i := ln1; i >= 0; i-- {
-		myNum = myNum + int(arr1[i]-'0')*pow
-		pow *= ln2
+	pw := 1
+	cnt := 0
+	for i := ln - 1; i >= 0; i-- {
+		cnt = cnt + st_t[[]rune(s)[i]]*pw
+		pw *= ln2
 	}
-
-	for myNum != 0 {
-		ans = string(baseTo[myNum%ln3]) + ans
-		myNum /= ln3
+	for cnt != 0 {
+		ans = string(p[cnt%ln3]) + ans
+		cnt /= ln3
 	}
-
 	return ans
 }
