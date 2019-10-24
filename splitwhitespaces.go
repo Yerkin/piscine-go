5f1525069e4b7aabff3881ed16cc25f4c20bdb79
yerkin <yerkin.abdukarimov@ce.sdu.edu.kz>
2019-10-24 17:14:11 +0600

    quest7-4

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -3,11 +3,18 @@ package piscine
 func SplitWhiteSpaces(str string) []string {
 	a := 0
 	ln := 0
-	for _, w := range str {
-		if w == '\n' || w == ' ' || w == '\t' {
+	ok2 := false
+	for c := range str {
+
+		if ok2 && c != 0 && (str[c-1] == '\n' || str[c-1] == '\t' || str[c-1] == ' ') && str[c] != '\n' && str[c] != '\t' && str[c] != ' ' {
 			ln++
 		}
+		if str[c] != '\n' && str[c] != '\t' && str[c] != ' ' {
+			ok2 = true
+		}
 	}
+	ln++
+
 	ans := make([]string, ln)
 	index := 0
 	myStr := ""
@@ -24,7 +31,7 @@ func SplitWhiteSpaces(str string) []string {
 		}
 	}
 
-	if str[a+1:] != "" {
+	if str[a+1:] != "" && str[a+1:] != " " {
 		ans[index] = str[a+1:]
 	}
 	return ans
